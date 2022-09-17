8b0400c38fc8894ad9d8429cd8d86dc02948c6ff
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-16 21:28:49 -0300

    Day 1

--- ten-days-of-javascript/day-1/1-3.js
+++ ten-days-of-javascript/day-1/1-3.js
@@ -1,8 +1,13 @@
 function main() {
     // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
     const PI = Math.PI;
+    const radius = parseInt(readLine());
     // Print the area of the circle:
+    const area = PI * Math.pow(radius, 2)
+    console.log(area);
     // Print the perimeter of the circle:
+    const perimeter = 2 * (PI * radius);
+    console.log(perimeter);
     try {    
         // Attempt to redefine the value of constant variable PI
         PI = 0;
