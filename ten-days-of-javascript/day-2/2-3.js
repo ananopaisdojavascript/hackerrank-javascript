905385d085094633db2b3cbfdf2867cdd5fad117
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-20 22:05:35 -0300

    Day 2: Loops

---
+++ ten-days-of-javascript/day-2/2-3.js
@@ -0,0 +1,26 @@
+const vowelsAndConsonants = (s) => {
+  const strToArr = Array.from(s);
+  const vowels = [];
+  const consonants = [];
+  for (let i = 0; i < strToArr.length; i++) {
+    if (
+      strToArr[i] === "a" ||
+      strToArr[i] === "e" ||
+      strToArr[i] === "i" ||
+      strToArr[i] === "o" ||
+      strToArr[i] === "u"
+    ) {
+      vowels.push(strToArr[i]);
+    } else {
+      consonants.push(strToArr[i]);
+    }
+  }
+  let concatArr = [...vowels, ...consonants];
+  for (let i of concatArr) {
+    console.log(i);
+  }
+}
+
+const str = "javascriptloops";
+
+vowelsAndConsonants(str);
