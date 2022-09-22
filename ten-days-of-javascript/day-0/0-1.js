1ebaa313850c5aea63b2c814cf841a2ff056eb37
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-21 21:31:58 -0300

    Day 0: Hello, World!

--- ten-days-of-javascript/day-0/0-1.js
+++
@@ -1,8 +0,0 @@
-const greeting = (parameterVariable) => {
-  // Primeiro console.log com a mensagem "Hello, World!"
-  console.log("Hello, World!");
-  // Segundo console.log com o parâmetro da função
-  console.log(parameterVariable);
-}
-
-greeting("Welcome to 10 Days of JavaScript!");
